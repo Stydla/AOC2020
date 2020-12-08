7df4fff072aef5d3978c2c9ffaaa75212f6f4c45
Ondřej Brožek <obrozek@centrum.cz>
2020-12-08 07:44:01 +0100

    8

---
+++ SolverAOC2020_8/Instruction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SolverAOC2020_8
+{
+  internal class Instruction
+  {
+    public Instruction(string name, int value)
+    {
+      Name = name;
+      Value = value;
+    }
+
+    public string Name { get; set; }
+    public int Value { get; }
+    public int ExecuteCount { get; private set; }
+
+    internal bool IsExecuted()
+    {
+      return ExecuteCount > 0;
+    }
+
+    internal void Execute(ref int instruction, ref long acumulator)
+    {
+      ExecuteCount++;
+      switch (Name)
+      {
+        case "nop":
+          {
+            instruction++;
+          }
+          break;
+        case "acc":
+          {
+            instruction++;
+            acumulator += Value;
+          }
+          break;
+        case "jmp":
+          {
+            instruction += Value;
+          }
+          break;
+        default:
+          throw new Exception($"Invalid instruciton {Name}");
+      }
+    }
+  }
+}
---
+++ SolverAOC2020_8/Program.cs
@@ -0,0 +1,94 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverAOC2020_8
+{
+  public class Program : BaseAdventSolver, IAdventSolver
+  {
+
+    public override string SolverName => "Day 8: Handheld Halting"/*TODO: Task Name*/;
+
+    public override string InputsFolderName => "SolverAOC2020_8";
+
+    public override string SolveTask1(string InputData)
+    {
+      List<Instruction> inst = ReadInstructions(InputData);
+
+      long acumulator = 0;
+      int instruction = 0;
+      
+      while(!inst[instruction].IsExecuted())
+      {
+        inst[instruction].Execute(ref instruction, ref acumulator);
+      }
+
+      return $"{acumulator}";
+    }
+
+    public override string SolveTask2(string InputData)
+    {
+
+      List<Instruction> inst2 = ReadInstructions(InputData);
+
+      for(int i = 0; i < inst2.Count; i++)
+      {
+        List<Instruction> inst = ReadInstructions(InputData);
+
+        if(inst[i].Name == "jmp")
+        {
+          inst[i].Name = "nop";
+        } else if (inst[i].Name == "nop")
+        {
+          inst[i].Name = "jmp";
+        } else
+        {
+          continue;
+        }
+
+        long acumulator = 0;
+        int instruction = 0;
+
+        while (!inst[instruction].IsExecuted())
+        {
+          inst[instruction].Execute(ref instruction, ref acumulator);
+
+          if(instruction >= inst.Count)
+          {
+            return $"{acumulator}";
+          }
+        }
+      }
+
+      return $"Not Found";
+
+      
+    }
+
+    private Instruction ReadInstruction(string line)
+    {
+      string name = line.Substring(0, 3);
+      int value = int.Parse(line.Substring(4));
+      return new Instruction(name, value);
+    }
+
+    private List<Instruction> ReadInstructions(string input)
+    {
+      List<Instruction> ret = new List<Instruction>();
+      using(StringReader sr = new StringReader(input))
+      {
+        string line;
+        while((line = sr.ReadLine()) != null)
+        {
+          var inst = ReadInstruction(line);
+          ret.Add(inst);
+        }
+      }
+      return ret;
+    }
+  }
+}
